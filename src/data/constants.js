9062bb241429967d87ff40e1c34f754d12b2932e
Shreyanshi200 <120807907+Shreyanshi200@users.noreply.github.com>
2024-04-28 06:01:23 +0530

    updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/Shreyanshi200",
     resume:
-      "https://drive.google.com/file/d/1ozyr2IR-L5_V763v4DyR2QO4RXDMf14K/view",
+      "https://drive.google.com/file/d/1cl25JCG6kzyCi4HrpLwC0kG6nCuJdLz-/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/shreyanshi-srivastava/",
     // twitter: "https://twitter.com/RishavChanda",
     insta: "https://www.instagram.com/shreyanshi_srivastava_1409/",
