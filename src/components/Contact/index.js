f6de7b0736888a7878f8025852212c15f6743ab9
Shreyanshi Srivastava <120807907+Shreyanshi200@users.noreply.github.com>
2024-06-23 15:25:54 +0530

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_pccbw5d', 'template_nflv74e', form.current, 'bmL1Mz7gLzI42JUFc')
       .then((result) => {
         setOpen(true);
         form.current.reset();
